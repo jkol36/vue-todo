455012c7395d8ef7ca3a58776c939d5f97397789
jon <jon@jons-MacBook-Pro.local>
2019-07-08 19:38:43 -0400

    basic functionality finished

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="app">
+    <Todos v-bind:todos="todos" />
+  </div>
+</template>
+
+<script>
+import Todos from './components/Todos.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Todos
+  },
+  data(){
+    return {
+      todos: [
+        {
+          id: '1',
+          title: 'To do one',
+          completed: false,
+        },{
+          id: '2',
+          title: 'To do two',
+          completed: true,
+        },
+        {
+          id: '3',
+          title: 'To do three',
+          completed: false,
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  text-rendering: optimizeLegibility;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  font-kerning: auto;
+}
+
+html {
+  font-family: sans-serif;
+  -webkit-text-size-adjust: 100%;
+}
+
+body {
+  margin: 0;
+}
+</style>
---
+++ src/components/Todos.vue
@@ -0,0 +1,22 @@
+<template>
+	<div> 
+		<div v-bind:key="todo.id" v-for="todo in todos"> 
+			<TodoItem v-bind:todo="todo"/>
+		</div>
+	</div>
+</template>
+
+<script> 
+	import TodoItem from './TodoItem.vue'
+	export default {
+		name: 'todos',
+		components: {
+			TodoItem
+		},
+		props: ["todos"]
+	}
+</script>
+
+<style scoped> 
+	
+</style>
