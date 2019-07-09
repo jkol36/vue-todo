46f93e2a17a51c64fbb98837ca252d1683c5580f
jon <jon@jons-MacBook-Pro.local>
2019-07-09 00:07:34 -0400

    add to do works

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <Header />
+    <Addtodo v-on:add-todo="addTodo" />
     <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
   </div>
 </template>
@@ -8,12 +9,14 @@
 <script>
 import Todos from './components/Todos.vue'
 import Header from './components/layout/Header'
+import Addtodo from './components/Addtodo'
 
 export default {
   name: 'app',
   components: {
     Todos,
-    Header
+    Header,
+    Addtodo
   },
   data(){
     return {
@@ -38,26 +41,44 @@ export default {
   methods: {
     deleteTodo(id) {
       this.todos = this.todos.filter(todo => todo.id !== id)
+    },
+    addTodo(newTodo) {
+      this.todos = [...this.todos, newTodo];
+
+
     }
   }
 }
 </script>
 
 <style>
-* {
-  box-sizing: border-box;
-  text-rendering: optimizeLegibility;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  font-kerning: auto;
-}
+  * {
+    box-sizing: border-box;
+    text-rendering: optimizeLegibility;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    font-kerning: auto;
+  }
 
-html {
-  font-family: sans-serif;
-  -webkit-text-size-adjust: 100%;
-}
+  html {
+    font-family: sans-serif;
+    -webkit-text-size-adjust: 100%;
+  }
 
-body {
-  margin: 0;
-}
+  body {
+    margin: 0;
+  }
+
+  .btn {
+    display: inline-block;
+    border: none;
+    background: #555;
+    color: #fff;
+    padding: 7px 20px;
+    cursor: pointer;
+
+  }
+  .btn:hover {
+    background: #666;
+  }
 </style>
---
+++ src/components/Addtodo.vue
@@ -0,0 +1,48 @@
+<template>
+	<div>
+		<form @submit="addTodo">
+			<input v-model="title" placeholder="add to do..."> </input>
+			<input class='btn' type="submit"></input>
+		</form>
+	</div>
+</template>
+
+<script>
+	import uuid from 'uuid'
+	export default {
+		name: "Addtodo",
+		data() {
+			return {
+				title: ''
+			}
+		},
+		methods: {
+			addTodo(e) {
+				e.preventDefault();
+				const newTodo = {
+					id: uuid.v4(),
+					title: this.title,
+					completed:false
+				}
+				//send up to parent
+				this.$emit('add-todo', newTodo);
+				this.title = ''
+
+			}
+		}
+	}
+</script>
+
+<style scoped> 
+	form {
+		display:flex;
+	}
+	input[type="text"] {
+		flex: 10;
+		padding:5px;
+	}
+
+	input[type="submit"] {
+		flex:2;
+	}
+</style>
